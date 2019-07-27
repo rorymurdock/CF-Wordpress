13f59573e3df73b08cfd39112509b769f92e43c6
Rory Murdock <rmurdock@woolworths.com.au>
2019-07-27 12:11:29 +1000

    Added environment checking

--- deploy.py
+++ deploy.py
@@ -1,7 +1,20 @@
 import boto3
+import argparse
+
+# Setup args
+PARSER = argparse.ArgumentParser()
+REQUIRED = PARSER.add_argument_group('Required arguments')
+OPTIONAL = PARSER.add_argument_group('Optional arguments')
+REQUIRED.add_argument("-env", help="Environment", required=True)
+ARGS = PARSER.parse_args()
+
+print('Deploying to %s environment' % ARGS.env)
 
 ec2 = boto3.client('ec2')
 
 response = ec2.describe_regions()
 print('Regions:')
 [print(region['RegionName']) for region in response['Regions']]
+
+
+# aws sts get-caller-identity
