01ff0062c3c85a6ede57fa186ecca89e1e4bcc7f
Rory Murdock <rorymurdock@users.noreply.github.com>
2019-07-30 19:44:38 +1000

    Development (#3)
    * Added DNS
    * Added wordpress tests
    * Linting
    * Renamed module to AWS
    * Added reqREST and lxml requirements
    * Updated README.md

---
+++ Tests/Wordpress_test.py
@@ -0,0 +1,22 @@
+import json
+import lxml.html
+from reqREST import REST
+
+with open('url.json') as json_file:
+    url = json.load(json_file)
+
+url = url['url']
+
+REST = REST(url=url, protocol='http')
+
+print('Using URL: %s' % url)
+
+# Sample tests
+# This could be branched out significantly
+
+def test_wordpress():
+    site = REST.get('')
+    assert site.status_code == 200
+
+    tree = lxml.html.fromstring(site.text)
+    assert tree.xpath('//title')[0].text_content() == "Blog Title – Just another WordPress site"
--- deploy.py
+++ deploy.py
@@ -1,7 +1,10 @@
+"""Deploy a CloudFormation template into AWS"""
+import sys
+import json
 import string
 import random
 import argparse
-from CFDeploy import wordpress
+from CFDeploy import AWS
 
 # Setup args
 PARSER = argparse.ArgumentParser()
@@ -10,17 +13,37 @@ OPTIONAL = PARSER.add_argument_group('Optional arguments')
 REQUIRED.add_argument("-env", help="Environment", required=True)
 ARGS = PARSER.parse_args()
 
+# Used for creating unique stack IDs
 def random_id(length=8):
     """Generate a random string of fixed length """
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for i in range(length))
 
+# Initialise function with random stack ID
+AWS = AWS('%s%s' % (ARGS.env, random_id()), ARGS.env)
 
-wp = wordpress('%s%s' % (ARGS.env, random_id()), ARGS.env)
-if wp.check_stack_exists():
-    wp.delete_stack()
+# Check for stack collisions
+if AWS.check_stack_exists():
+    # If delete stacks is enabled the stack will be deleted
+    if not AWS.delete_stack():
+        print('Unable to delete conflicting stack')
+        sys.exit(1)
 
-wp.create_stack()
+# Create the stack
+AWS.create_stack()
 
-wp.check_stack_created(10, 100)
+# Wait for the stack to initialise
+AWS.check_stack_created(10, 100)
 
-print('URL: %s' % wp.get_output_url())
+# Output the URL for this new stack
+print('URL: %s' % AWS.get_output_url())
+
+# Save locally for tests to pick up
+URL = {}
+URL['url'] = AWS.get_output_url()
+
+# Removes any trailing dots
+url = "StagingJE0ZJYUH.staging.itmatic.com.au.".rstrip('.')
+
+# Write to json file for pytests to pick up
+with open('url.json', 'w') as outfile:
+    json.dump(URL, outfile)
